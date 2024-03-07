cfd2dee03e8b3d03fd775beca6a3a53b95c83f9a
Yaw Akoto <yawakoto.python@gmail.com>
2024-03-07 01:15:43 +0000

    TDD tests update to the home page

--- stopsearch_api/manual.py
+++ stopsearch_api/manual.py
@@ -4,14 +4,31 @@ app = Flask(__name__)
 
 @app.route("/docs")
 def manual():
+    pages = {
+        "0": "/",
+        "1": "/docs",
+        "2": "/home",
+        "3": "/create/report/",
+        "4": "/find/by/victim",
+        "5": "/find/by/witness"
+    }
     create_report = {}
     find_report_by_age = {}
     documentation = {
-        "App": "Stop Search UK",
-        "Description": "An app developed to record and report incidents between the public and the police.",
-        "Rerdord an incident": create_report,
-        "Find Report By": {
-            "Age": find_report_by_age,
-        }
+        "App": "StopSearch UK",
+        "Year": "2024",
+        "Description": "An app developed to record and report incidents between the police and the public.",
+        "Founder": "Daniella Rose + Akoto Tech",
+        "AppPage": "/docs",
+        "Pages": [
+            {
+                "Index": pages["0"],
+                "Manual": pages["1"],
+                "HomePage": pages["2"],
+                "CreateReport": pages["3"],
+                "FindReportByVictim": pages["4"],
+                "FindReportByWitness": pages["5"],
+            }
+        ]
     }
     return jsonify(documentation, {"status": 200})
