ba758217099f42611adbb4c5def289c7e74cd3a2
PythonAkoto <yawakoto.python@example.com>
2024-05-03 00:23:21 +0100

    filtered searches on Map route

--- StopSearchUK/main.py
+++ StopSearchUK/main.py
@@ -6,9 +6,7 @@ from StopSearchUK.stopsearch_api.demo_route import demo_route
 from StopSearchUK.stopsearch_api.report_demo_route import report_demo_route
 # minimal viable product - MVP
 from StopSearchUK.stopsearch_api.new_report_route import new_report_route
-from StopSearchUK.stopsearch_frontend.report_page import report_page
 from StopSearchUK.stopsearch_api.map_route import map_data
-from StopSearchUK.stopsearch_frontend.map_page import stopsearch_map_page
 from StopSearchUK.stopsearch_api.search_engine_route import (
     search_all_reports_route, 
     search_reports_by_data_id_route, 
@@ -22,7 +20,13 @@ from StopSearchUK.stopsearch_api.search_engine_route import (
     search_report_by_search_by_6_months,
     search_report_by_search_by_12_months,
     search_report_by_search_by_30_days,
-) 
+)
+from StopSearchUK.stopsearch_frontend.report_page import report_page
+from StopSearchUK.stopsearch_frontend.map_page import (
+    stopsearch_map_page,
+    show_map_by_data_id,
+)
+
 
 
 app.route("/")(index_route)
@@ -30,11 +34,13 @@ app.route("/docs")(manual_route)
 app.route("/home")(home_route)
 app.route("/new/")(report_demo_route)
 app.route("/demo")(demo_route)
+
 # minimal viable product - MVP
 app.route("/new/report")(new_report_route)
 app.route("/report")(report_page)
 app.route("/map-data")(map_data)
 app.route("/map")(stopsearch_map_page)
+app.route("/map/<data_id>")(show_map_by_data_id)
 app.route("/search/all")(search_all_reports_route)
 app.route("/search/<data_id>")(search_reports_by_data_id_route)
 app.route("/search/formType/<form_type>")(search_report_by_form_type)
